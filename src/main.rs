89fe37cca47eeb2957753515acde805f4c35c7d7
Mathidot <90128272+mathidot@users.noreply.github.com>
2026-01-30 16:45:31 +0800

    Implement mock engine mode with subscribed channels
    Refactor Polynomial Trading System to support mock engine mode with subscribed channels.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use dotenvy::dotenv;
+use futures::future::try_join;
+use polynomial::DEFAULT_BASE_URL;
 use polynomial::DEFAULT_CHAIN_ID;
-use polynomial::data_engine::DataEngine;
+use polynomial::DataEngine;
+use polynomial::SubscribedChannel;
+use polynomial::WssChannelType;
+use polynomial::config::EngineMode;
 use polynomial::errors::Result;
+use polynomial::stream::MockStream;
 use polynomial::{ClobClient, FillEngine, config};
 use polynomial::{GlobalState, execute_egine::ExecuteEgine};
 use rust_decimal_macros::dec;
@@ -35,30 +41,52 @@ async fn main() -> Result<()> {
 
     tracing::info!("🚀 Starting Polynomial Trading System...");
 
-    // load config
-    let private_key = env::var("PK").expect("PRIVATE_KEY must be set in .env file");
-    let strategy_config = config::load_strategy_config();
-
+    let engine_config = config::load_engine_config();
     let global_state = Arc::new(GlobalState::new());
     let (tx, rx) = mpsc::unbounded_channel();
-    let data_engine = Arc::new(DataEngine::new(global_state.clone(), tx).await);
-    let client = ClobClient::with_l1_headers(
-        "https://clob.polymarket.com",
-        &private_key,
-        DEFAULT_CHAIN_ID,
-    );
-    let fill_engine = FillEngine::new(dec!(10), dec!(0.5), 0);
-    let mut execute_egine =
-        ExecuteEgine::new(client, rx, strategy_config, fill_engine, global_state);
+    let mut subscribed_channels = Vec::new();
+
+    match engine_config.engine_mode {
+        EngineMode::MOCK => {
+            subscribed_channels.push(SubscribedChannel {
+                chan_type: WssChannelType::Crypto,
+                chan_stream: MockStream::new(10000),
+            });
+
+            subscribed_channels.push(SubscribedChannel {
+                chan_type: WssChannelType::Sports,
+                chan_stream: MockStream::new(10000),
+            });
+
+            subscribed_channels.push(SubscribedChannel {
+                chan_type: WssChannelType::User,
+                chan_stream: MockStream::new(10000),
+            });
+        }
+        EngineMode::REAL => {}
+    }
+
+    let data_engine =
+        Arc::new(DataEngine::new(global_state.clone(), tx, subscribed_channels).await);
     let data_engine_handle = tokio::spawn(async move {
         data_engine.run();
     });
 
-    let execute_engine_handle = tokio::spawn(async move {
-        execute_egine.run().await;
-    });
+    let _ = tokio::try_join!(data_engine_handle);
 
-    let _ = tokio::try_join!(data_engine_handle, execute_engine_handle);
+    // load config
+    // let private_key = env::var("PK").expect("PRIVATE_KEY must be set in .env file");
+    // let strategy_config = config::load_strategy_config();
+    // let client = ClobClient::with_l1_headers(DEFAULT_BASE_URL, &private_key, DEFAULT_CHAIN_ID);
+    // let fill_engine = FillEngine::new(dec!(10), dec!(0.5), 0);
+    // let mut execute_egine =
+    //     ExecuteEgine::new(client, rx, strategy_config, fill_engine, global_state);
+
+    // let execute_engine_handle = tokio::spawn(async move {
+    //     execute_egine.run().await;
+    // });
+
+    // let _ = tokio::try_join!(data_engine_handle, execute_engine_handle);
 
     tokio::signal::ctrl_c()
         .await
